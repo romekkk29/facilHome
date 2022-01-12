2e54fe7ac20853d4d6e37db34941b45a86abba29
romekkk29 <romegomez29@gmail.com>
2022-01-12 12:32:38 -0300

    nose

---
+++ js/admin.js
@@ -0,0 +1,21 @@
+
+    //declarando variables
+let body =document.querySelector("body");
+let logo1=document.querySelector("#logo1");
+let form=document.querySelector("form");
+let button=document.querySelector("button");
+let header=document.querySelector("header");
+
+//pagina de inicio con TIMEOUT
+setTimeout(function(){
+    body.classList.remove("setTime");
+    logo1.classList.add("hidden");
+
+},1500);
+setTimeout(function(){
+    form.classList.remove("hidden"); 
+    button.classList.remove("hidden"); 
+    header.style.backgroundColor="white";
+},2500);
+
+
